b2acfb5ea8660d45fe66a9166274c80ecfd93e90
Вова Барибін <oknalogia@outlook.com>
2021-10-01 13:49:33 +0300

    created contract and deploy function

--- smart-ang/src/app/app.component.ts
+++ smart-ang/src/app/app.component.ts
@@ -11,39 +11,49 @@ const ethereum = window.ethereum;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public account: string;
-  private balance: string;
-  public read: string;
-  private contractAddr: string;
-  private contract: any;
-  public message: string;
+  public account:        string;
+  private _balance:      string;
+  public read:           string;
+  private _contractAddr: string;
+  private _contract:     any;
+  public message:        string;
+  public Contract:       any;
+  private _provider:     any;
+  private _byteCode:     any;
+  private _signer:       any;
 
 
   constructor(
     private _http: HttpClient
   ) {
     this.account = '';
-    this.balance = '';
+    this._balance = '';
     this.read = '';
-    this.contractAddr = '0xd9145CCE52D386f254917e481eB44e9943F39138';
+    this._contractAddr = '0xd9145CCE52D386f254917e481eB44e9943F39138';
     this.message = '';
   }
 
-  ngOnInit() {
-    this.contract = this._http.get("../assets/contracts/HelloWorld.json");
-    //console.log(JSON.stringify(this.contract));
+  async ngOnInit() {
+    this._contract =  require("../assets/contracts/HelloWorld.json");
+    this._byteCode =  require("../assets/contracts/HelloWorldBC.json");
+    this._provider =  new ethers.providers.Web3Provider(window.ethereum);
+    this._signer   =  this._provider.getSigner();
+    //this.contract = this._http.get("../assets/contracts/HelloWorldalt.json");
+    //console.log(this._contract.abi);
+
+    //this.message = await HelloWorld.message;
   }
 
   async getAccount() {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     this.account = accounts[0]; //showAcc
 
-    this.balance = await ethereum.request({
+    this._balance = await ethereum.request({
       method: 'eth_getBalance',
       params: [this.account, 'latest']
     })
     //showBalance
-    this.read = (parseInt(this.balance) / 10 ** 18).toFixed(5);
+    this.read = (parseInt(this._balance) / 10 ** 18).toFixed(5);
   }
   sendEth() {
     ethereum.request({
@@ -51,12 +61,30 @@ export class AppComponent implements OnInit {
       params: [
         {
           from: this.account,
-          to: this.contractAddr,
+          to: this._contractAddr,
         }
 
       ],
     })
-    .then((txHash: any) => console.log(txHash))
-    .catch((error: any) => console.log(error.message))
+      .then((txHash: any) => console.log(txHash))
+      .catch((error: any) => console.log(error.message))
+  }
+  async readData() {
+    const helloWorldContract = this.createInstance(this._contractAddr);
+    console.log(helloWorldContract.message());
+    this.message = await helloWorldContract.message;
+  }
+
+  createInstance(contractAddr: string) {
+    console.log(new ethers.Contract(contractAddr, this._contract.abi, this._signer));
+    return  new ethers.Contract(contractAddr, this._contract.abi, this._signer);
+  }
+  getContractFromFactory() {
+    return ethers.ContractFactory.fromSolidity(this._contract).connect(this._signer);
+  }
+  async getContractAddr(){
+    const HelloWorld = await this.getContractFromFactory().deploy('Hello World!');
+    console.log(HelloWorld);
+    this._contractAddr = HelloWorld.address;
   }
 }
