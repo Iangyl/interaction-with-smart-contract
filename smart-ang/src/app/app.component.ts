2fbd3c9d4dbcd4123bd3e4aeb77dcde405ec7692
Вова Барибін <oknalogia@outlook.com>
2021-10-01 03:29:12 +0300

    added contracts and wrote a script

--- smart-ang/src/app/app.component.ts
+++ smart-ang/src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ethers } from "ethers";
+declare let window: any;
+
+const ethereum = window.ethereum;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'smart-ang';
+export class AppComponent implements OnInit {
+  public account: string;
+  private balance: string;
+  public read: string;
+  private contractAddr: string;
+  private contract: any;
+  public message: string;
+
+
+  constructor(
+    private _http: HttpClient
+  ) {
+    this.account = '';
+    this.balance = '';
+    this.read = '';
+    this.contractAddr = '0xd9145CCE52D386f254917e481eB44e9943F39138';
+    this.message = '';
+  }
+
+  ngOnInit() {
+    this.contract = this._http.get("../assets/contracts/HelloWorld.json");
+    //console.log(JSON.stringify(this.contract));
+  }
+
+  async getAccount() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    this.account = accounts[0]; //showAcc
+
+    this.balance = await ethereum.request({
+      method: 'eth_getBalance',
+      params: [this.account, 'latest']
+    })
+    //showBalance
+    this.read = (parseInt(this.balance) / 10 ** 18).toFixed(5);
+  }
+
+  createInstance() {
+    return new ethers.Contract(this.contract, this.contractAddr);
+  }
+
+  async getData() {
+    const helloWorldContract = this.createInstance();
+    this.message = await helloWorldContract.methods.message().call();
+  }
+
 }
